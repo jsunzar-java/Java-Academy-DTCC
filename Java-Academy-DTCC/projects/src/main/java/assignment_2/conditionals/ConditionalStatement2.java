d6eabea67874dd4923c273de119c9a8c8d474b47
jsunzar-java <84419398+jsunzar-java@users.noreply.github.com>
2021-06-18 02:06:29 -0400

    Add files via upload

---
+++ Java-Academy-DTCC/projects/src/main/java/assignment_2/conditionals/ConditionalStatement2.java
@@ -0,0 +1,34 @@
+package assignment_2.conditionals;
+
+public class ConditionalStatement2 {
+    public static void main (String args[]) {
+        int value = 87;
+        if(value !=100) {
+                System.out.println("You Win");
+            doSomething(true);
+            }
+         else {
+            System.out.println("You Lose");
+            doSomething(false);
+        }
+    }
+
+
+    public static void doSomething(boolean b) {
+        if(b) {
+            System.out.println("You win again");
+        }
+
+        if(b != false) {
+            System.out.println("You win, again, and again");
+        }
+
+        if(b || b) {
+            System.out.println("I think you would probably always win here");
+        }
+
+        if(!b && !b ) {
+            System.out.println("You will never win here!");
+        }
+    }
+}
